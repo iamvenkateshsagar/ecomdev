42d841dad7cffee7393d4f0583f42943f2bacec3
iamvenkateshsagar <iamvenkateshsagar@gmail.com>
2026-07-19 16:57:06 +0530

    issue fixed

--- versions.tf
+++ versions.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.8"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   cloud {
     organization = "venkatsagario"
 
