882eab09419b0871a1f97941bd5b00e62185e749
iamvenkateshsagar <iamvenkateshsagar@gmail.com>
2026-07-20 05:54:15 +0530

    secrets manager module created

--- main.tf
+++ main.tf
@@ -6,3 +6,23 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
 
 }
+
+module "secrets_manager" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "~> 1.3"
+
+  name        = "dev/db/credentials"
+  description = "Database credentials"
+
+  secret_string = jsonencode({
+    username = "dbadmin"
+    password = "ChangeMeAtRuntime123!"
+  })
+
+  recovery_window_in_days = 7
+
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "terraform"
+  }
+}
